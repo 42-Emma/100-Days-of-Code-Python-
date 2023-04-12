ec461f17001124d226ec8ec44191122036109bdb
42-Emma <ogunemmanuella2@gmail.com>
2023-04-12 11:24:48 +0100

    created band name generator

---
+++ Day 1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What is the name of the city you live?\n").capitalize()
+pet = input("What is the name of your pet?\n").capitalize()
+print("Your band name could be " + city + " " + pet)
